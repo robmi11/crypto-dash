60bfac214f9195194bc15fc8d5e5065034b2e5ae
robmi11 <robert.mizianty@gmail.com>
2025-09-28 13:29:36 +0200

    Added Routes: home, about, not found.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { Routes, Route } from "react-router";
+import AboutPage from "./pages/about";
 import HomePage from "./pages/home";
+import Header from "./components/Header";
+import NotFoundPage from "./pages/not-found";
 
 export default function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<HomePage />}
-      />
-      <Route
-        path="about"
-        element={<h1>About Page</h1>}
-      />
-      <Route
-        path="*"
-        element={<h1>Page couldn't be found'</h1>}
-      />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="about"
+          element={<AboutPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </>
   );
 }
